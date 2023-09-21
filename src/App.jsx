63df146a838e3d365b8d2fcb200aa6acde6fc1ea
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-09-21 10:56:11 +0530

    brower router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
-
+import {BrowserRouter} from "react-router-dom"
 const App=()=> {
 
   return (
     <>
-        <h1>welcome amit</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<h1>shared layout</h1>}>
+              <Route path="/about" element={<h2>about</h2>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
